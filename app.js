cb1bff25eef6eb6857ec31f47762806ce418fd5c
miladsiddiquey <75581636+miladsiddiquey@users.noreply.github.com>
2021-02-09 01:58:34 +0600

    hard-rock-project

---
+++ app.js
@@ -0,0 +1,61 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/:${searchText}`
+    //load data
+    fetch(url)
+        .then(rest => rest.json())
+        .then(data => displaySong(data.data))
+        //search kore na paile error dekabe
+        .catch(error=>displayError('something went wrong! please try again later.'));
+}
+
+const displaySong = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML='';
+    songs.forEach(song => {
+        
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+
+        songDiv.innerHTML = `
+        
+                    <div class="col-md-9">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+
+                        <audio controls>
+                        <source src="${song.preview}" type="audio/mpeg"
+                        </audio> 
+
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+                    </div>
+        `;
+        songContainer.appendChild(songDiv);
+
+    });
+}
+
+
+const getLyrics = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    //load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+    //search kore na paile error dekabe
+    .catch(error=>displayError('something went wrong! please try again later.'));
+}
+
+const displayLyrics = lyrics => {
+    
+    const lyricsDiv = document.getElementById('song-lyrics');
+    
+    lyricsDiv.innerText = lyrics;
+}
+
+const displayError=error=>{
+    const errorTag=document.getElementById('error-message');
+    errorTag.innerText=error;
+}
